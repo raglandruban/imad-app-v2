1dba5a1907f64a91e0c3362e51d5ad3176c1d195
ragland <ragland444ruban@gmail.com>
2017-02-17 18:51:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,15 +10,15 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article one',function (req,res) {
-    res.send("article one under progress");
+    res.send("article one under process");
 });
 
 app.get('/article two',function (req,res) {
-    res.send("article two under progress");
+    res.send("article two under process");
 });
 
 app.get('/article three',function (req,res) {
-    res.send("article three under progress");
+    res.send("article three under process");
 });
 
 app.get('/ui/style.css', function (req, res) {
