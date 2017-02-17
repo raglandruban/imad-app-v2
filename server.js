6ffbb4ddba8a2c9ed60ef33f7182bcca683cddf9
ragland <ragland444ruban@gmail.com>
2017-02-17 18:53:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,15 +10,15 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article one',function (req,res) {
-    res.send("article one under process");
+    res.send('article one under process');
 });
 
 app.get('/article two',function (req,res) {
-    res.send("article two under process");
+    res.send('article two under process');
 });
 
 app.get('/article three',function (req,res) {
-    res.send("article three under process");
+    res.send('article three under process');
 });
 
 app.get('/ui/style.css', function (req, res) {
@@ -28,7 +28,6 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
