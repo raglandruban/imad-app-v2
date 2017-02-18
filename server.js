c7a7ef21e9cdb7488ccde34a93ae1acc98e8bf6f
ragland <ragland444ruban@gmail.com>
2017-02-18 21:36:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,14 +9,6 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
- app.get('/articleone',function (req,res) {
-     res.send('articleone is under construction')
- });
- 
-  app.get('/articletwo',function (req,res) {
-     res.send('articletwo is under construction')
- });
- 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
