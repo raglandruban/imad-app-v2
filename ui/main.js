8f5fe223ae62eb3e56480c50e9bab15c1c6f1adb
ragland <ragland444ruban@gmail.com>
2017-02-24 16:13:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,12 +6,6 @@ element.innerHTML = "MY WEBAPP,RAGLAND";
 
 
 var img = document.getElementById("madi");
- var marginleft = 0;
- function moveright ()
- {
-     marginleft = marginleft + 1;
-     img.style.marginleft = marginleft + 'px';
- }
- img.onclick = function () {
-     var interval = setInterval(moveright, 50);
- };
+img.onclick = function () {
+    img.style.marginleft = '100px';
+};
