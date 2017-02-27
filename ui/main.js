bfb1a3e50bd7599a89e49d58f01026bdb3c16db7
ragland <ragland444ruban@gmail.com>
2017-02-27 12:04:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,11 @@
 //counter code
 
-var button = document.getElementById("counter");
+var button = document.getElementById('counter');
 var counter = 0;
 
 button.Onclick = function () {
     counter = counter + 1;
-    var span = document.getElementById("count");
+    var span = document.getElementById('count');
     span.innerHTML = counter.toString();
 };
 
