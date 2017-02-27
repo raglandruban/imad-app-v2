2a56d3eda18ea42a089441549bc27f9ab70f0655
ragland <ragland444ruban@gmail.com>
2017-02-27 12:08:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,22 @@
 //counter code
 
 var button = document.getElementById('counter');
-var counter = 0;
+
 
 button.Onclick = function () {
-    counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
+    var request = new XMLHttpRequest();
+    
+     requset.onreadystatechange = function () {
+      if (request.readyState === XMLHttpRequest.DONE) 
+      {
+          if (request.status === 200) {
+              var counter = request.responseText;
+              var span = document.getElementById('count');
+              span.innerHTML = counter.toString();
+          }
+      }
+    };
+    
+    request.open('GET', 'http://raglandruban.imad.hasura-app.io/counter', true);
+    request.send(null);
 };
-
-
